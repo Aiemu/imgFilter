04ab40986430c37a453d6741907b4ab54570d38d
ZengZheng <zengz17@outlook.com>
2021-11-19 16:38:36 +0800

    finish compile on OSX

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # imgFilter
-include some image filters, such as lomo and crystallization.
+
+## Overview
+Use C++ to implement LOMO and Crystallization filters. The structure of the project and its description are as follows:
+```
+```
+
+## Usage
+### Test Environment and Dependent Library
+- `C++ 14`
+- `cmake 3.21.4`
+- `gcc/g++`: Apple clang version 13.0.0
+- OS: macOS Monterey, 12.0.1
+- IDE: CLion 2021.2.3
+
+### Quick Start
+You can test the effect of the filters by directly clicking on the executable file (`bin/*.exe`) and running the source code (`src/`). Specific steps are as follows.
+
+#### From `bin/`
+
+
+#### From `src/`
+
+
+## Filter Implement
+### LOMO
+### Crystallization
+
+## References
--- main.cpp
+++ main.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <opencv2/opencv.hpp>
 
 #include "include/SLIC.h"
 #include "include/LOMO.h"
 
 using namespace cv;
+using namespace std;
+
+struct Config {
+    int M;
+    int cluster_num;
+    std::string path;
+};
+
+Config get_config();
 
 int main() {
-    const char *path = "../data/lenna.png";
-//    LOMO lomo(path);
-//    lomo.set_saturation();
-//    lomo.display_saturation();
-//    lomo.add_dark_halo();
-//    lomo.display_lomo_result();
-//    waitKey(0);
+//    const char *path = "../data/tch.jpg";
+    Config config = get_config();
+    LOMO lomo(config.path.c_str());
+    lomo.set_saturation();
+    lomo.display_saturation();
+    lomo.add_dark_halo();
+    lomo.display_lomo_result();
 
-    SLIC slic(1600, path, 15);
+    SLIC slic(config.cluster_num, config.path.c_str(), config.M);
     int loop = 10;
     for (int i = 0; i < loop; i++) {
         std::cout << "Loop: " << i + 1 << std::endl;
@@ -26,4 +37,23 @@ int main() {
     }
 
     return 0;
+}
+
+Config get_config() {
+    ifstream fin;
+    fin.open("../config.txt", ios::in);
+    std::string tmp_line;
+    Config ret;
+    while (getline(fin, tmp_line)) {
+        tmp_line.erase(std::remove(tmp_line.begin(), tmp_line.end(), ' '), tmp_line.end());
+        std::string content = tmp_line.substr(tmp_line.find("=") + 1);
+        if (tmp_line.find("filename") == 0) {
+            ret.path = content;
+        } else if (tmp_line.find("M") == 0) {
+            ret.M = stoi(content);
+        } else if (tmp_line.find("cluster") == 0) {
+            ret.cluster_num = stoi(content);
+        }
+    }
+    return ret;
 }
